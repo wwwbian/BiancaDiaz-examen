80a9ebc67fb478e2679e46e7f98dcbde9edab639
wwwbian <bdiaz@escuelasproa.edu.ar>
2023-10-31 11:24:10 -0300

    no sé

--- 1.gd
+++ 1.gd
@@ -2,7 +2,7 @@ extends Node
 func sumOdds(array,imp): 
     for i in range(array.size()):
          if  array[i] % 2 == 1:
-                imp[i] += array[i]
+                array[i] += imp[i]
         
 func _ready():
     var array = [2,1,4,6,3]
